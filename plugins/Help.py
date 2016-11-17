603ce2301c5385dec7b25653be74c7383078aa96
Mick Schellekens <mick.schellekens@hetnet.nl>
2016-11-17 13:05:03 +0100

    Removed idea map from tracked files

--- plugins/Help.py
+++ plugins/Help.py
@@ -8,6 +8,10 @@ class Plugin(object):
 	async def HandleCommand(self, messageObject, command, args):
 		if command == "help":
 			await self.help(messageObject)
+		if command == "info":
+			await self.info(messageObject)
 			
 	async def help(self, messageObject):
 		tmp = await self.pm.client.send_message(messageObject.channel, 'Helphelp')
+	async def info(self, messageObject):
+		tmp = await self.pm.client.send_message(messageObject.channel, 'Poke Dynista or Theraga for help')
