ea29ec0f040cf3894191e5749eb3095f140229b7
Mick Schellekens <mick.schellekens@hetnet.nl>
2016-11-17 13:50:17 +0100

    setup for mentioning users
    when sending !hello. bot should send hello @user_that_invoked_command.

--- plugins/Help.py
+++ plugins/Help.py
@@ -3,15 +3,25 @@ class Plugin(object):
 		self.pm = pm
 		
 	def RegisterCommands(self):
-		return ["help", "info", "halppl0x"]
+		return ["help", "info", "hello"]
 		
 	async def HandleCommand(self, messageObject, command, args):
 		if command == "help":
 			await self.help(messageObject)
 		if command == "info":
 			await self.info(messageObject)
+		if command == "hello":
+			await self.info(messageObject)
 			
 	async def help(self, messageObject):
 		tmp = await self.pm.client.send_message(messageObject.channel, 'Helphelp')
 	async def info(self, messageObject):
 		tmp = await self.pm.client.send_message(messageObject.channel, 'Poke Dynista or Theraga for help')
+
+	async def hello(self, messageObject):
+		if self.pm.client.message.author == self.pm.client.user:
+			return
+
+		else:
+			msg = 'Hello {0.author.mention}'.format(messageObject)
+			await self.pm.client.send_message(messageObject.channel, msg)
