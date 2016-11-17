a7da863b37f95bb1895270ab5162916ccb94a687
Carlos Lubbers <carloslubbers111@gmail.com>
2016-11-17 12:46:47 +0100

    Ping delay

--- plugins/Ping.py
+++ plugins/Ping.py
@@ -1,13 +1,15 @@
+import datetime
 class Plugin(object):
 	def __init__(self, pm):
 		self.pm = pm
 		
 	def RegisterCommands(self):
-		return []
+		return ["ping", "pong"]
 		
 	async def HandleCommand(self, messageObject, command, args):
 		if command == "ping":
 			await self.ping(messageObject)
 			
 	async def ping(self, messageObject):
-		tmp = await self.pm.client.send_message(messageObject.channel, 'Pong')
+		speed = datetime.datetime.now() - messageObject.timestamp
+		tmp = await self.pm.client.send_message(messageObject.channel, "Pong "+ str(round(speed.microseconds/1000)) + "ms")
