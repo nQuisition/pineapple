bb68865a3aa2a2836c2588fd8ffd6d172a39101e
Carlos Lubbers <carloslubbers111@gmail.com>
2016-11-21 13:25:41 +0100

    Support for multi-character command prefixes, a bit of documentation

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 from PluginManager import PluginManager
 import discord
-import sys
 
-print("Starting UNNAMEDBOT")
+print("Starting Pineapple")
 print("Starting Discord Client")
+# Creates a discord client, which we will use to connect and interact with the server.
+# All methods with @client.event annotations are event handlers for this client.
 client = discord.Client()
+
 print("Loading plugins")
+# Loads and initializes the plugin manager for the bot
 pm = PluginManager("plugins/", client)
 pm.load_plugins()
 pm.register_events()
@@ -14,23 +17,39 @@ print("Plugins loaded and registered")
 
 @client.event
 async def on_ready():
+    """
+    Event handler, fires when the bot has connected and is logged in
+    """
     print('Logged in as ' + client.user.name + " (" + client.user.id + ")")
+
+    # Change nickname to nickname in configuration
     for instance in client.servers:
         await client.change_nickname(instance.me, pm.botPreferences.nickName)
 
 
 @client.event
 async def on_message(message):
+    """
+    Event handler, fires when a message is received in the server.
+    :param message: discord.Message object containing the received message
+    """
     try:
         if message.content.startswith(pm.botPreferences.commandPrefix):
+            # Send the received message off to the Plugin Manager to handle the command
             words = message.content.partition(' ')
-            await pm.handle_command(message, words[0][1:], words[1:])
+            await pm.handle_command(message, words[0][len(pm.botPreferences.commandPrefix):], words[1:])
     except Exception as e:
         await client.send_message(message.channel, "Error: " + str(e))
 
 
 @client.event
 async def on_typing(channel, user, when):
+    """
+    Event handler, fires when a user is typing in a channel
+    :param channel: discord.Channel object containing channel information
+    :param user: discord.Member object containing the user information
+    :param when: datetime timestamp
+    """
     try:
         await pm.handle_typing(channel, user, when)
     except Exception as e:
@@ -39,10 +58,16 @@ async def on_typing(channel, user, when):
 
 @client.event
 async def on_message_delete(message):
+    """
+    Event handler, fires when a message is deleted
+    :param message: discord.Message object containing the deleted message
+    """
     try:
         if message.author.name != "PluginBot":
             await pm.handle_message_delete(message)
     except Exception as e:
         await client.send_message(message.channel, "Error: " + str(e))
 
+
+# Run the client and login with the bot token (yes, this needs to be down here)
 client.run(pm.botPreferences.token)
--- plugins/Empty.py
+++ plugins/Empty.py
@@ -7,11 +7,25 @@ class Plugin(object):
 
     @staticmethod
     def register_events():
+        """
+        Define events that this plugin will listen to
+        :return: A list of util.Events
+        """
         return [Events.Command("example_command")]
 
     async def handle_command(self, message_object, command, args):
-        if command == "ping":
-            await self.ping(message_object)
+        """
+        Handle Events.Command events
+        :param message_object: discord.Message object containing the message
+        :param command: The name of the command (first word in the message, without prefix)
+        :param args: List of words in the message
+        """
+        if command == "example_command":
+            await self.example_command(message_object)
 
-    async def nick(self, message_object):
+    async def example_command(self, message_object):
+        """
+        Execute the example_command command. All calls to self.pm.client should be asynchronous (await)!
+        :param message_object: discord.Message object
+        """
         await self.pm.client.send_message(message_object.channel, 'Pong')
