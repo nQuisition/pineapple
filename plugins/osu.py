12161335e4555ef5bbf5d963a67eab42b923b3cd
Carlos Lubbers <carloslubbers111@gmail.com>
2017-01-02 21:37:52 +0100

    Small osu improvement

--- plugins/osu.py
+++ plugins/osu.py
@@ -1,19 +1,17 @@
 import requests
 import urllib.request
-import errno
 import os
-
 from util import Events
 
 
 class Plugin(object):
     def __init__(self, pm):
         self.pm = pm
-        self.apikey = self.pm.botPreferences.get_config_value("OSU", "apikey")
+        self.api_key = self.pm.botPreferences.get_config_value("OSU", "apikey")
 
     @staticmethod
     def register_events():
-        return [Events.Command("osu",desc="Get the osu details for a user")]
+        return [Events.Command("osu", desc="Get the osu details for a user")]
 
     async def handle_command(self, message_object, command, args):
         if command == "osu":
@@ -27,17 +25,17 @@ class Plugin(object):
                 os.makedirs(directory)
             filename = os.path.join(directory, username + "test.jpg")
             urllib.request.urlretrieve("http://lemmmy.pw/osusig/sig.php?colour=pink&uname=" + username, filename)
-            apikey = self.apikey
-            url = 'https://osu.ppy.sh/api/get_user?k=' + apikey + '&u=' + username
+            api_key = self.api_key
+            url = 'https://osu.ppy.sh/api/get_user?k=' + api_key + '&u=' + username
             response = requests.get(url, verify=True)
-            displayData = response.json()[0]
+            display_data = response.json()[0]
             await self.pm.client.send_file(message_object.channel, filename)
             await self.pm.client.send_message(message_object.channel,
-                                              "username: " + displayData["username"] + "\n" + "rank: " + displayData[
-                                                  "pp_rank"] +"\n" + "accuracy: " + displayData[
-                                                  "accuracy"] + "\n" + "pp: " + displayData[
-                                                  "pp_raw"] + "\n" + "country: " + displayData[
-                                                  "country"] + "\n" + "Rank in country: " + displayData[
+                                              "Username: " + display_data["username"] + "\n" + "Rank: " + display_data[
+                                                  "pp_rank"] + "\n" + "Accuracy: " + display_data[
+                                                  "accuracy"] + "\n" + "PP: " + display_data[
+                                                  "pp_raw"] + "\n" + "Country: " + display_data[
+                                                  "country"] + "\n" + "Rank in country: " + display_data[
                                                   "pp_country_rank"])
             os.remove(filename)
         except:
