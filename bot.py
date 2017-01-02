833da0e1556c5c129a792e07c46e9a36bb55bffe
Mick Schellekens <mick.schellekens@hetnet.nl>
2017-01-02 14:20:45 +0100

    Added a playing message to the bot

--- bot.py
+++ bot.py
@@ -26,6 +26,7 @@ async def on_ready():
     # Change nickname to nickname in configuration
     for instance in client.servers:
         await client.change_nickname(instance.me, pm.botPreferences.nickName)
+    await client.change_presence(game=discord.Game(name='Use >help for help'))
 
 
 @client.event
