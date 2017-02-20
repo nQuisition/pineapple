0b641e717b869ed722fb1b3ed34acb821be419cf
Carlos Lubbers <carloslubbers111@gmail.com>
2017-02-20 19:38:44 +0100

    :thinking:

--- plugins/JoinLeave.py
+++ plugins/JoinLeave.py
@@ -5,6 +5,7 @@ import random
 import asyncio
 from util.Ranks import Ranks
 
+
 class Plugin(object):
     def __init__(self, pm):
         self.pm = pm
@@ -12,7 +13,9 @@ class Plugin(object):
 
     @staticmethod
     def register_events():
-        return [Events.UserJoin("welcome_msg"), Events.UserLeave("leave_msg"), Events.Command("togglewelcome", rank=Ranks.Admin)]
+        return [Events.UserJoin("welcome_msg"), Events.UserLeave("leave_msg"),
+                Events.Command("togglewelcome", rank=Ranks.Admin),
+                Events.Command("ban", rank=Ranks.Mod)]
 
     async def handle_member_join(self, member):
         if self.enabled:
@@ -36,3 +39,17 @@ class Plugin(object):
         if command == "togglewelcome":
             self.enabled = not self.enabled
             await self.pm.client.delete_message(message_object)
+            await self.pm.client.send_message(message_object.channel, "Welcome messages: **" + str(self.enabled) + "**")
+        if command == "ban":
+            if len(message_object.mentions) == 1:
+                leave = glob.glob(os.getcwd() + "/images/" + "bye.gif")
+                file = random.choice(leave)
+                await asyncio.sleep(1)
+                msg1 = await self.pm.client.send_message(message_object.channel, "Bye " + message_object.mentions[0].name)
+                msg2 = await self.pm.client.send_file(message_object.channel, file)
+
+                await asyncio.sleep(5)
+                await self.pm.client.delete_message(message_object)
+                await self.pm.client.delete_message(msg1)
+                await self.pm.client.delete_message(msg2)
+                await self.pm.client.send_message(message_object.channel, "Nah, just kidding.")
