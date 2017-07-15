e746a748a3bddd266ef5b8e6caf62349ec8a5c04
mikusHusbando <siryx97@gmail.com>
2017-07-15 11:42:35 +0200

    heavily commented

--- plugins/Exhentai.py
+++ plugins/Exhentai.py
@@ -5,10 +5,12 @@ import re
 
 from util import Events
 
+# TODO: FIND OUT HOW TO CREATE JSON OBJECT FROM REQUEST, ACCESS JSON DATA
+
 # API-URL and type of headers sent by POST-request
 url = "https://api.e-hentai.org/api.php"
 json_request_headers = "{'content-type': 'application/json'}"
-
+# json_request_headers = "{'content-type': 'application/json; charset=UTF-8'}"
 
 class Plugin(object):
     def __init__(self, pm):
@@ -19,35 +21,37 @@ class Plugin(object):
     def register_events():
         return [Events.Message("Exhentai")]
 
+    @staticmethod
+    def build_payload(gallery_id, gallery_token):
+        return '{"method": "gdata","gidlist": [[' + gallery_id + ',"' + gallery_token + '"]],"namespace": 1}'
+
     async def handle_message(self, message_object):
         # Genius RegEx for finding all exhentai-links formatted like copied from exh adressbar
-        exhentailinks = re.findall(r'(https://exhentai.org/g/([0-9]+)/([0-9a-f]{10})/)', message_object.content)
+        regex_result_list = re.findall(r'(https://exhentai.org/g/([0-9]+)/([0-9a-f]{10})/)', message_object.content)
         # loop over all found links that match genius RegEx
-        for entry in exhentailinks:
+        for tuple in regex_result_list:
             # TODO: CACHE ALREADY DOWNLOADED HEADERS WITH TIMESTAMP
             # Key missing, or incorrect key provided.-Error has to catched
             # actually everything matching the RegEx will never return something but HTTP 200
 
             # Setting the 2nd and 3rd tuple value of the RegEx to properly named variables.
             # These tuples are automatically generated from the re.findall-command with given RegEx
-            gallery_id = entry[1]
-            gallery_token = entry[2]
+            gallery_id = tuple[1]
+            gallery_token = tuple[2]
 
             # TODO: IGNORE DOUBLE LOOKUP LIST ENTRIES WITH SAME HEADER / SAME ID&TOKEN
             # maybe even cache API results for some time
 
             # TODO: refactor
             # This is the required JSON-string for requesting data from the E-Hentai API
-            payload = (
-                '{"method": "gdata","gidlist": [[' + gallery_id + ',"' + gallery_token + '"]],"namespace": 1}')
 
-            response = requests.post(url, payload, json_request_headers)
+            response = requests.post(url, self.build_payload(gallery_id,gallery_token), json_request_headers)
             # print a pretty formatted JSON-String to chat containing info about the given galleries
 
             # TODO: BUILD PRETTY LINK TEMPLATE
-            await self.pm.client.send_message(self.name, message_object.channel,
-                                              json.dumps(response.json(), sort_keys=True, indent=4))
-
+            # await self.pm.client.send_message(message_object.channel,json.dumps(response.json(), sort_keys=True, indent=4))
+            await self.pm.clientWrap.send_message(self.name , message_object.channel,json.dumps(response.json(), sort_keys=True, indent=4))
+            # await self.pm.clientWrap.send_message(self.name , message_object.channel,json.dumps(response.json(), sort_keys=True, indent=4),False)
             # https://exhentai.org/g/66211541/d5c164c9b6/ error gallery not found
             # https://exhentai.org/g/108711/5f57a5eb11/ error wrong gallery token
             #
