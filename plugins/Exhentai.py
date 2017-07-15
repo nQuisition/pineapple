db4bb3c8d9d1b43b2157b92487482054ecbecfb5
mikusHusbando <siryx97@gmail.com>
2017-07-15 18:26:49 +0200

    print newline after printing tag section

--- plugins/Exhentai.py
+++ plugins/Exhentai.py
@@ -21,25 +21,28 @@ class Plugin(object):
         return [Events.Message("Exhentai")]
 
     async def handle_message(self, message_object):
+
         # Genius RegEx for finding all exhentai-links formatted like copied from exh adressbar
         # maybe broaden it up to match e-hentai / http / written www if still good performance-wise
         regex_result_list = re.findall(r'(https://exhentai.org/g/([0-9]+)/([0-9a-f]{10})/)', message_object.content)
 
         # loop over all found links that match genius RegEx
+        # 3 API message api send requests per link, might get out of hand
         for link_tuple in regex_result_list:
+
             # TODO: (Core) Print error on wrong link
-            # TODO: (Performance) Do not download API Data twice, maybe even cache but thats hardly worth the time
+            # TODO: (Performance) Do not download API Data twice, maybe even cache API data but thats hardly worth the time
             # Key missing, or incorrect key provided.-Error has to catched
             # Gallery not found. If you just added this gallery, [...]-Error has to get catched
             # but everything matching the RegEx will never return something but HTTP 200
-            # maybe if server is down
-
+            # maybe if server is down 404 or the regex is broadened you will get 301 / 302 for moved if www/non-www forwarding happens
 
             # Setting the 2nd and 3rd tuple value of the RegEx to properly named variables.
             # These tuples are automatically generated from the re.findall-command with given RegEx
             # the regex_result_list contains of a tuple with the different substrings in it
             # link_tuple[0] is the whole string
             # link_tuple[1] and [2] are the first and second matched substrings
+
             gallery_id = link_tuple[1]
             gallery_token = link_tuple[2]
 
@@ -48,9 +51,8 @@ class Plugin(object):
             # create json from Response using built-in parser
             json_data = response.json()
 
-            # TODO: (core) Make the taglist look pretty when printed
-
             # Debug-Snippets
+
             # print whole json
             # await self.pm.client.send_message(message_object.channel,json.dumps(response.json(), sort_keys=True, indent=4))
 
@@ -58,14 +60,15 @@ class Plugin(object):
             # outstring = pprint.pformat(json_data, indent=4)
             # await self.pm.clientWrap.send_message(self.name, message_object.channel, outstring)
 
+            # Build the title-message
             await self.pm.clientWrap.send_message(self.name, message_object.channel, self.build_title_string(
                 json_data) + "\n" + self.build_title_jpn_string(json_data) + "\n")
-            await self.pm.client.send_message(message_object.channel, json_data['gmetadata'][0]['thumb'])
-            await self.pm.clientWrap.send_message(self.name, message_object.channel,
-                                                  pprint.pformat(json_data['gmetadata'][0]['tags']))
 
-            # maybe put some kind of line-seperator in between 2 posts
-            # await self.pm.client.send_message(message_object.channel, "<seperator>\n")
+            # Send the cover as its own String (non-embed) for preview
+            await self.pm.client.send_message(message_object.channel, json_data['gmetadata'][0]['thumb'])
+
+            # print message including tag-section
+            await self.pm.clientWrap.send_message(self.name, message_object.channel, self.build_tag_section() + "\n")
 
     @staticmethod
     def build_payload(gallery_id, gallery_token):
@@ -78,3 +81,9 @@ class Plugin(object):
     @staticmethod
     def build_title_jpn_string(json_data):
         return 'Japanese Title: ' + pprint.pformat(json_data['gmetadata'][0]['title_jpn'])
+
+    # TODO: (core) Make the taglist look pretty when printed
+    @staticmethod
+    def build_tag_section(json_data):
+        return pprint.pformat(json_data['gmetadata'][0]['tags'])
+
