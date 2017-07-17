ca336ddbbb0a6006cebf9d1efc2d83dd7c1b9ddd
mikusHusbando <siryx97@gmail.com>
2017-07-17 02:17:49 +0200

    error checks implemented

--- plugins/Exhentai.py
+++ plugins/Exhentai.py
@@ -61,10 +61,6 @@ class Plugin(object):
         regex_result_list_unique = list(tuple(self.return_unique_set(regex_result_list)))
 
         for link_tuple in regex_result_list_unique:
-            # TODO: (Core) Catch json "field 'title' not found"-error on wrong link
-            # Key missing, or incorrect key provided.-Error has to catched
-            # Gallery not found. If you just added this gallery, [...]-Error has to get catched
-            # only possible to check by evaluating reponse json
 
             # Setting the 2nd last and last tuple value of the RegEx to properly named variables.
             # the regex_result_list contains of a tuple with the different substrings in it
@@ -77,16 +73,18 @@ class Plugin(object):
             json_data = requests.post(api_url, self.build_payload(gallery_id, gallery_token),
                                       json_request_headers).json()
 
-            # Build the title-message
-            await self.pm.clientWrap.send_message(self.name, message_object.channel, self.build_title_string(
-                json_data) + "\n" + self.build_title_jpn_string(json_data) + "\n")
+            if not 'error' in json_data['gmetadata'][0]:
+                # Build the title-message
+                await self.pm.clientWrap.send_message(self.name, message_object.channel, self.build_title_string(
+                    json_data) + "\n" + self.build_title_jpn_string(json_data) + "\n")
 
-            # Send the cover as its own String (non-embed) for preview
-            await self.pm.client.send_message(message_object.channel, json_data['gmetadata'][0]['thumb'])
+                # Send the cover as its own String (non-embed) for preview
+                await self.pm.client.send_message(message_object.channel, json_data['gmetadata'][0]['thumb'])
 
-            # print message including tag-section
-            await self.pm.clientWrap.send_message(self.name, message_object.channel,
-                                                  self.build_tag_section(json_data) + "\n")
+                # print message including tag-section
+                await self.pm.clientWrap.send_message(self.name, message_object.channel,
+                                                      self.build_tag_section(json_data) + "\n")
+
 
     @staticmethod
     def build_payload(gallery_id, gallery_token):
@@ -99,11 +97,11 @@ class Plugin(object):
     # TODO: (core) Don't print empty string for japanese title if none given
     @staticmethod
     def build_title_jpn_string(json_data):
-        return 'Japanese Title: ' + pprint.pformat(json_data['gmetadata'][0]['title_jpn'])
+        if 'title_jpn' in json_data['gmetadata'][0]:
+            return 'Japanese Title: ' + pprint.pformat(json_data['gmetadata'][0]['title_jpn'])
 
     # TODO: (core) Make the taglist look pretty when printed
     @staticmethod
     def build_tag_section(json_data):
-        # the list returned by accessing the json-data at that tags-key will get each element extracted
-        # ", " will be printed to seperate them and the tags written like male:shotacon will get replaced to male : shotacon
+        # ", " is the line separator. Tags (eg. "female:schoolgirl") will be added spaces around the ":"
         return (", ".join(json_data['gmetadata'][0]['tags'])).replace(":", " : ")
