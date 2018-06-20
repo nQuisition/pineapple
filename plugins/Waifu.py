d208ac399728d3e8dac6660b3f462c365a9aa60c
kennyko <kennyko28@gmail.com>
2018-06-20 00:03:54 -0700

    Fix Waifu bot to bypass security (#12)

--- plugins/Waifu.py
+++ plugins/Waifu.py
@@ -24,7 +24,8 @@ class Plugin(object):
             await self.get_waifu(message_object, "random")
 
     async def get_waifu(self, message_object, path):
-        waifu_request = requests.get("https://mywaifulist.moe/{}".format(path))
+        waifu_headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36"}
+        waifu_request = requests.get("https://mywaifulist.moe/{}".format(path), headers=waifu_headers)
         if(waifu_request.status_code != 200):
             print("Non-200 response from mywaifulist.moe:\n{}".format(waifu_request.content))
             await self.waifu_error_message(message_object)
@@ -32,7 +33,8 @@ class Plugin(object):
 
         try:
             soup = bs(waifu_request.content, "html.parser")
-            waifu = json.loads(soup.find("waifucore")["waifu-json"])
+            waifu_id = json.loads(soup.find("waifucore")["waifu-id"])
+            waifu = self.call_waifu_api(waifu_id)
             embed = discord.Embed()
             embed.set_image(url=waifu["display_picture"])
         except KeyError:
@@ -43,6 +45,17 @@ class Plugin(object):
         await self.pm.client.send_message(message_object.channel,
                                           "Your waifu is **{}** from **{}**".format(waifu["name"], waifu["series"]["name"]),
                                           embed = embed)
+
+    def call_waifu_api(self, waifu_id):
+        waifu_headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36",
+            "x-requested-with": "XMLHttpRequest",
+        }
+        waifu_api_request = requests.get("https://mywaifulist.moe/api/waifu/{}".format(waifu_id), headers=waifu_headers)
+        if(waifu_api_request.status_code != 200):
+            print("Non-200 response from mywaifulist.moe:\n{}".format(waifu_api_request.content))
+            return {}
+        return json.loads(waifu_api_request.content)
 
     async def waifu_error_message(self, message_object):
         await self.pm.client.send_message(message_object.channel,
