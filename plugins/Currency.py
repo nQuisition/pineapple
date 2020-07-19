e0eeebe20d17274d7623b9a1f0a4c253f994d023
nQ <nquisition@gmail.com>
2020-07-19 22:12:42 +0900

    Added ability to add/remove/list currency aliases. (#35)
    Additionally, aliases can now contain whitespaces. This means that more messages will match to the regex in handle_message, but I don't think it's a problem?

--- plugins/Currency.py
+++ plugins/Currency.py
@@ -6,9 +6,10 @@ from typing import Dict
 from asyncio import Lock
 from aiohttp import ClientSession
 from util import Events
+from util.Ranks import Ranks
 from AbstractPlugin import AbstractPlugin
 
-currency_aliases = {
+default_currency_aliases = {
     '$': 'USD',
     'NT$': 'TWD',
     'RMB': 'CNY',
@@ -28,6 +29,10 @@ class InvalidCacheException(Exception):
     pass
 
 
+class JsonFileException(Exception):
+    pass
+
+
 class FixerApiException(Exception):
     def __init__(self, message):
         super().__init__('Fixer API error: ' + message)
@@ -39,7 +44,9 @@ class Plugin(AbstractPlugin):
     lock: Lock
     api_key: str
     cache_path: str
+    currency_aliases_path: str
     rates: Dict[str, float]
+    currency_aliases: Dict[str, str]
     last_updated: int
     update_interval: int
 
@@ -47,17 +54,52 @@ class Plugin(AbstractPlugin):
         super().__init__(pm, "Currency")
         self.api_key = self.pm.botPreferences.get_config_value("Fixer", "apikey")
         self.cache_path = os.path.join(pm.cache_dir, 'currency.json')
+        self.currency_aliases_path = os.path.join(pm.cache_dir, 'currency_aliases.json')
         self.update_interval = 60*60*6  # 6 hours
         self.session = ClientSession()
         self.lock = Lock()
+        self.read_aliases_file()
 
     @staticmethod
     def register_events():
-        return [Events.Message("Currency")]
+        return [Events.Message("Currency"),
+                Events.Command("addcurrency", rank=Ranks.Mod, desc="Add currency alias"),
+                Events.Command("removecurrency", rank=Ranks.Mod, desc="Remove currency alias"),
+                Events.Command("listcurrencies", desc="List all known currency aliases")]
+
+    # TODO move to utils or something?
+    @staticmethod
+    def read_json_file(path):
+        if not os.path.exists(path):
+            raise JsonFileException
+        with open(path) as json_file:
+            try:
+                return json.load(json_file)
+            except json.JSONDecodeError:
+                # invalid JSON
+                raise JsonFileException
+
+    async def handle_command(self, message_object, command, args):
+        if command == 'addcurrency':
+            upper = args[1].upper()
+            parsed = re.match(r'^\s*([A-Z€£¥$ ]+)\s+([A-Z]+)\s*$', upper)
+            if not parsed:
+                await self.pm.clientWrap.send_message(self.name, message_object.channel,
+                                                      'Incorrect argument supplied, the format is `addcurrency '
+                                                      '<alias> <target>`')
+                return
+            alias = parsed.group(1)
+            target = parsed.group(2)
+            await self.add_currency_alias(message_object, alias, target)
+        elif command == 'removecurrency':
+            alias = args[1].upper()
+            await self.remove_currency_alias(message_object, alias)
+        elif command == 'listcurrencies':
+            await self.list_currency_aliases(message_object)
 
     async def handle_message(self, message_object):
         upper = message_object.content.upper()
-        parsed = re.match(r'^\s*([0-9]+(?:\.[0-9]*)?)\s*([A-Z€£¥$]+)\s+(?:TO|IN)\s+([A-Z€£¥$]+)\s*$', upper)
+        parsed = re.match(r'^\s*([0-9]+(?:\.[0-9]*)?)\s*([A-Z€£¥$ ]+)\s+(?:TO|IN)\s+([A-Z€£¥$ ]+)\s*$', upper)
         if not parsed:
             return
         amount = float(parsed.group(1))
@@ -74,18 +116,24 @@ class Plugin(AbstractPlugin):
             return False
 
     def read_cache_file(self):
-        if not os.path.exists(self.cache_path):
-            raise InvalidCacheException
-        with open(self.cache_path) as json_file:
-            try:
-                cache_data = json.load(json_file)
-                self.last_updated = cache_data['_updated']
-                self.rates = cache_data['rates']
-                if not self.is_cache_valid():
-                    raise InvalidCacheException
-            except json.JSONDecodeError:
-                # invalid JSON, regenerate
+        try:
+            cache_data = self.read_json_file(self.cache_path)
+            self.last_updated = cache_data['_updated']
+            self.rates = cache_data['rates']
+            if not self.is_cache_valid():
                 raise InvalidCacheException
+        except JsonFileException:
+            raise InvalidCacheException
+
+    def read_aliases_file(self):
+        try:
+            self.currency_aliases = self.read_json_file(self.currency_aliases_path)
+        except JsonFileException:
+            self.currency_aliases = default_currency_aliases
+
+    def save_currency_aliases(self):
+        with open(self.currency_aliases_path, 'w') as json_file:
+            json.dump(self.currency_aliases, json_file)
 
     async def update_cache(self):
         if self.is_cache_valid():
@@ -112,13 +160,13 @@ class Plugin(AbstractPlugin):
             proper_from_currency = from_currency
             proper_to_currency = to_currency
             if proper_from_currency not in self.rates:
-                if proper_from_currency not in currency_aliases:
+                if proper_from_currency not in self.currency_aliases:
                     return
-                proper_from_currency = currency_aliases[proper_from_currency]
+                proper_from_currency = self.currency_aliases[proper_from_currency]
             if proper_to_currency not in self.rates:
-                if proper_to_currency not in currency_aliases:
+                if proper_to_currency not in self.currency_aliases:
                     return
-                proper_to_currency = currency_aliases[proper_to_currency]
+                proper_to_currency = self.currency_aliases[proper_to_currency]
             total = amount * self.rates[proper_to_currency] / self.rates[proper_from_currency]
 
             response_text = f'{amount} **{proper_from_currency}** = {total:.2f} **{proper_to_currency}**'
@@ -127,3 +175,46 @@ class Plugin(AbstractPlugin):
         except Exception as e:
             print(e)
             # do nothing
+
+    async def add_currency_alias(self, message_object, alias, target_currency):
+        try:
+            # populate local cache in case it is empty
+            await self.update_cache()
+            # I'm not sure if we should be checking this?
+            if target_currency not in self.rates:
+                await self.pm.clientWrap.send_message(self.name, message_object.channel,
+                                                      f'Unknown currency **{target_currency}**')
+                return
+            # If alias was already defined it will overwrite it, OK I guess
+            self.currency_aliases[alias] = target_currency
+            self.save_currency_aliases()
+            await self.pm.clientWrap.send_message(self.name, message_object.channel,
+                                                  f'**{alias}** is now an alias of **{target_currency}**')
+        except Exception as e:
+            print(e)
+            await self.pm.clientWrap.send_message(self.name, message_object.channel,
+                                                  f'Error adding alias: {e}')
+
+    async def remove_currency_alias(self, message_object, alias):
+        if alias not in self.currency_aliases:
+            await self.pm.clientWrap.send_message(self.name, message_object.channel,
+                                                  f'**{alias}** is not a known alias')
+            return
+        # If alias was already defined it will overwrite it, OK I guess
+        del self.currency_aliases[alias]
+        self.save_currency_aliases()
+        await self.pm.clientWrap.send_message(self.name, message_object.channel,
+                                              f'Alias **{alias}** successfully deleted')
+
+    async def list_currency_aliases(self, message_object):
+        aliases_by_currency = {}
+        for alias in self.currency_aliases:
+            target = self.currency_aliases[alias]
+            if target not in aliases_by_currency:
+                aliases_by_currency[target] = []
+            aliases_by_currency[target].append(alias)
+        lines = []
+        for currency, aliases in aliases_by_currency.items():
+            aliases.sort()
+            lines.append(f'{currency}: {", ".join(aliases)}')
+        await self.pm.clientWrap.send_message(self.name, message_object.channel, '```' + '\n'.join(lines) + '```')
